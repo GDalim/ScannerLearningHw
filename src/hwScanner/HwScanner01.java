2d0ba50f54606a3ed8dbbcf883d7b9b479dc7d1b
GDalim <gabrieldalim@gmail.com>
2022-05-12 15:55:38 -0700

    ScannerLearningHw

---
+++ src/hwScanner/HwScanner01.java
@@ -0,0 +1,36 @@
+package hwScanner;
+
+import java.util.Scanner;
+
+public class HwScanner01 {
+
+    public void doSummation(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the first number");
+        int num1 = input.nextInt();
+        System.out.println("Enter the second number");
+        int num2 = input.nextInt();
+        int total = num1+num2;
+        System.out.println("Total Summation : "+total);
+
+    }
+
+    public void doSubtraction(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the first number");
+        int num1 = input.nextInt();
+        System.out.println("Enter the second number");
+        int num2 = input.nextInt();
+        int total = num1-num2;
+        System.out.println("Total Subtraction : "+total);
+
+    }
+
+    public static void main(String[] args) {
+        HwScanner01 hi = new HwScanner01();
+        hi.doSummation();
+        hi.doSubtraction();
+
+
+    }
+}
